a56588cc17194f20e5fe0de31f9a25a6024ab800
Eugene Bilyy <bilyy@mlsdev.com>
2020-07-28 09:42:59 +0300

    test url

--- router/index.js
+++ router/index.js
@@ -1,11 +1,16 @@
 const Router = require("@koa/router");
 const router = new Router();
 const controller = require("../controllers/post");
+const { DB_URL } = require("../app/config");
 
 router.get("/articles", controller.getAll);
 router.get("/articles/:id", controller.getById);
 router.post("/articles", controller.create);
 router.put("/articles/:id", controller.update);
 router.delete("/articles/:id", controller.deleteById);
-
+router.get("/", (ctx, next) => {
+  ctx.body = {
+    url: DB_URL,
+  };
+});
 module.exports = router;
