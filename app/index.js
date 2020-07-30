2190697a4a933659c9044840eb168c816b13dac1
Eugene Bilyy <bilyy@mlsdev.com>
2020-07-30 22:24:08 +0300

    add some updates and rename some files

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-// const mongoose = require('mongoose');
-// const runDb = require('./db/index')
-
 const serve = require("koa-static");
 const router = require("../router");
 // runDb()
